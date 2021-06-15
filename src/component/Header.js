04f8df3862dbe346a6d2b2e0bb9ab2ef30fea436
imene joubeli <joubeliimen@gmail.com>
2021-06-15 18:11:52 +0100

    first Update

---
+++ src/component/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import '../App.css'
+
+const Header = () => {
+    return (
+        <div className='Header'>
+            <div className='imageHeader'>
+                <img style={{ marginTop: '10px' }} width='94px' height='65px' src='./user.png' alt='logo' />
+                <p><b>Axios Campany</b></p>
+            </div>
+           
+               
+
+
+        
+        </div>
+    )
+}
+
+export default Header
